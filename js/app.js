c78a8f951aca80640f1a3955cdefddf204c52ecb
Eric <8BitRain@gmail.com>
2023-02-14 07:59:52 -0600

    Truncating logs by removing json log format
    Previously JSON stringify was used to take JSON objects and append them to the data field of the reported google analytics. Now we just use the string. So {"name": "The name of the element"} is now reported as The name of the element.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,26 @@
 $(document).ready(function(){
     console.log(document.title)
     //Telemetry Event
-	var data = {
-		"pageName" : document.title,
-	};
-    googleAnalyticsTelemetry.LogPageView("onView", JSON.stringify(data));
+	var data = document.title;
+    googleAnalyticsTelemetry.LogPageView("onView", data);
 });
 
 function onPDFDownload(domElement)
 {
     console.log(domElement.name);
     //Telemetry Event
-	var data = {
-		"pdf" : domElement.name
-	};
+	var data = domElement.name;
 
-	googleAnalyticsTelemetry.LogResourceDownload("onPDFDownload", JSON.stringify(data));
+	googleAnalyticsTelemetry.LogResourceDownload("onPDFDownload", data);
 }
 
 function onClickEvent(domElement)
 {
     console.log(domElement.name);
     //Telemetry Event
-	var data = {
-		"element" : domElement.name
-	};
+	var data = domElement.name
 
-	googleAnalyticsTelemetry.LogClick("onClick", JSON.stringify(data));
+	googleAnalyticsTelemetry.LogClick("onClick", data);
 }
 
 function onFAQViewed(domElement)
